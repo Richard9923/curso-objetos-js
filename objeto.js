fff0043e1fc5ea4c9cd22575cc3961953bd52dfd
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-24 19:08:36 -0300

    inicio

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = [111, 222, 333];
+
+const informacoesPessoa = ['nome', 'Jose', 'idade', 32, 'CPF', '1212121212'];
+
+const informacoesPessoaOBJ = {
+    nome: 'Jose',
+    idade: 32,
+    CPF: '1212121212',
+}
+
+
